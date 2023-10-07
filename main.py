aa646517d53bd624317ec6fd73c0b9415b0b9a25
Vlad21islav <vmitk@mail.ru>
2023-10-07 11:32:14 +0300

    start

---
+++ main.py
@@ -0,0 +1,27 @@
+from random import *
+from pygame import *
+
+
+init()
+
+screen = display.set_mode(flags=RESIZABLE)  # flags=NOFRAME
+display.set_caption('')
+icon = image.load('').convert_alpha()
+display.set_icon('')
+
+w, h = display.get_surface().get_size()
+
+running = True
+while running:
+
+    screen.fill('black')
+
+
+
+    display.update()
+
+    keys = key.get_pressed()
+    for event in event.get():
+        if event.type == QUIT or keys[K_ESCAPE]:
+            running = False
+            quit()
